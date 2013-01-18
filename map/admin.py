109e4e9d7df95cca8793ef7909ac652b053d0b4a
Witchakorn Kamolpornwijit <chalet16@gmail.com>
2013-01-18 13:18:13 +0700

    Fix Location Admin page for people to use

--- map/admin.py
+++ map/admin.py
@@ -2,6 +2,6 @@ from map.models import Location
 from django.contrib import admin
 
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('title','url','position')
+    list_display = ('location','company','title','description','url')
 
 admin.site.register(Location,LocationAdmin)
