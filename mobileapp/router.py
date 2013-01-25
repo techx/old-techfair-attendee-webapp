f76afc23c8d596e99190aac13ea00c28d70ff275
Witchakorn Kamolpornwijit <chalet16@gmail.com>
2013-01-26 01:10:56 +0700

    Remove router read/write duplication

--- mobileapp/router.py
+++ mobileapp/router.py
@@ -3,21 +3,24 @@ class Router(object):
     A router to control all database operations on models in the
     auth application.
     """
-    def db_for_read(self, model, **hints):
+    
+    def _check_using(self,mode,model,**hints):
+        using = None
         if hasattr(model,'using'):
-            return model.using
-        return None
+            using =  model.using
+        return using
+    
+    def db_for_read(self, model, **hints):
+        return self._check_using('read',model, **hints)
 
     def db_for_write(self, model, **hints):
         """
         Attempts to read auth models go to auth_db.
         """
-        if hasattr(model,'using'):
-            return model.using
-        return None
-
+        return self._check_using('write',model, **hints)
+        
     def allow_relation(self, obj1, obj2, **hints):
-        return None
+        return True
 
     def allow_syncdb(self, db, model):
         return None
