2f50a58dbce5cefdb667bd286c6602ea016dc07d
Witchakorn Kamolpornwijit <chalet16@gmail.com>
2013-01-26 01:10:29 +0700

    Hack to fix company selection bug

--- map/models.py
+++ map/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from company.models import Company
 # Create your models here.
+class CompanyField(models.ForeignKey):
+    def validate(self, value, model_instance):
+        return
+
 class Location(models.Model):
     location= models.CharField(max_length=200)
-    company = models.ForeignKey(Company,blank=True,null=True)
+    company = CompanyField(Company,blank=True,null=True)
     title = models.CharField(max_length=200,blank=True)
     url = models.CharField(max_length=200,blank=True)
     description = models.TextField(blank=True)
